cf3f004dd404b3fb643bce744e530fb578d87246
Christakis <banzachristakis@gmail.com>
2024-10-29 18:53:55 -0300

    Add Seller Class

--- demo-dao-jdbc/src/main/java/org/example/application/Program.java
+++ demo-dao-jdbc/src/main/java/org/example/application/Program.java
@@ -1,12 +1,18 @@
 package org.example.application;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1,"Book");
-        System.out.println(department);
+
+        Seller seller = new Seller(1500.0, 2, "Bob", "Bob@gmail.com", LocalDate.now(), department);
+
+        System.out.println(seller);
 
     }
 }
