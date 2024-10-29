6fc2cfc9c006cf071e7f39257acb5f742451de5e
Christakis <banzachristakis@gmail.com>
2024-10-29 18:39:44 -0300

    Add Departement Class

---
+++ demo-dao-jdbc/src/main/java/org/example/application/Program.java
@@ -0,0 +1,12 @@
+package org.example.application;
+
+import org.example.model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department department = new Department(1,"Book");
+        System.out.println(department);
+
+    }
+}
