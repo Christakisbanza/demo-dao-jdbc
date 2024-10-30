f49917914826aa4fa964102bf7fae84a9a1c96d1
Christakis <banzachristakis@gmail.com>
2024-10-29 22:32:49 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/main/java/org/example/application/Program.java
+++ demo-dao-jdbc/src/main/java/org/example/application/Program.java
@@ -1,5 +1,7 @@
 package org.example.application;
 
+import org.example.model.dao.DaoFactory;
+import org.example.model.dao.SellerDao;
 import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department department = new Department(1,"Book");
 
         Seller seller = new Seller(1500.0, 2, "Bob", "Bob@gmail.com", LocalDate.now(), department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
--- demo-dao-jdbc/src/main/java/org/example/model/dao/SellerDao.java
+++ demo-dao-jdbc/src/main/java/org/example/model/dao/SellerDao.java
@@ -2,7 +2,6 @@ package org.example.model.dao;
 
 
 import org.example.model.entities.Seller;
-
 import java.util.List;
 
 public interface SellerDao {
@@ -12,5 +11,5 @@ public interface SellerDao {
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
-    
+
 }
