0ca64c1e20fabe5f9d1b80fd07f3dbea3b888644
Christakis <banzachristakis@gmail.com>
2024-10-30 15:54:58 -0300

    FindByDepartment implementation

--- demo-dao-jdbc/src/main/java/org/example/application/Program.java
+++ demo-dao-jdbc/src/main/java/org/example/application/Program.java
@@ -2,7 +2,10 @@ package org.example.application;
 
 import org.example.model.dao.DaoFactory;
 import org.example.model.dao.SellerDao;
+import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
+
+import java.util.List;
 
 
 public class Program {
@@ -15,5 +18,9 @@ public class Program {
 
         System.out.println(seller);
 
+        System.out.println("\n==== Test 2: seller findById =====");
+        List<Seller> sellers = sellerDao.findByDepartment(new Department(2,null));
+
+        sellers.forEach(System.out::println);
     }
 }
